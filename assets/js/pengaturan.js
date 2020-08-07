a180d0bf75fca7f45d73e924d3d5b11abe9d0ddd
Josep Marcello <josep1403@students.itb.ac.id>
2020-08-07 19:43:31 +0700

    Nambahin pengaturan pengguna

---
+++ assets/js/pengaturan.js
@@ -0,0 +1,9 @@
+// Ngisi form di pengaturan profil
+document.querySelector("#nama-depan").value = "Benten";
+document.querySelector("#nama-belakang").value = "Cucumber";
+document.querySelector("#laki-laki").checked = true;
+document.querySelector("#biografi").innerHTML = "Mahasiswa / Pengajar";
+document.querySelector("#socmed-1").value = "https://jspmarc.my.id"
+document.querySelector("#socmed-2").value = "https://facebook.com/9INTERNETEXPLORER9"
+document.querySelector("#socmed-3").value = "https://instagram.com/jspmarc"
+document.querySelector("#socmed-5").value = "https://linkedin.com/in/josepmk1"
